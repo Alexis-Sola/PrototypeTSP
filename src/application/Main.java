f188fe9b6a2862327622504f89806d0854a1c2ad
Exisow <exisow@pop-os.localdomain>
2021-05-05 14:04:23 +0200

    execution fxml main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,46 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	
+	  private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("HomeView"), 640, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
 	
 	public static void main(String[] args) {
-		launch(args);
+		launch();
+		/*Graph graph = new SingleGraph("Tutorial 1");
+
+		graph.addNode("A" );
+		graph.addNode("B" );
+		graph.addNode("C" );
+		graph.addEdge("AB", "A", "B");
+		graph.addEdge("BC", "B", "C");
+		graph.addEdge("CA", "C", "A");
+		graph.display();*/
+
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module Prototypage {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires gs.core;
+	requires javafx.fxml;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, gs.core;
 }

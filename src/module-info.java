82e258d350dc288ebe3eec5d76857f0243a21e87
Alexis Sola <exisow@pop-os.localdomain>
2022-01-20 21:04:57 +0100

    first commit

--- src/module-info.java
+++ src/module-info.java
@@ -1,15 +1,10 @@
 module Prototypage {
 	requires javafx.controls;
-	requires javafx.graphics;
 	requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.media;
-	requires javafx.swing;
-	requires javafx.web;
-	requires javafx.swt;
 	requires gs.core;
 	requires gs.ui.javafx;
 	requires TSPModel.PtiDeb;
 	
 	opens application to javafx.graphics, javafx.fxml, gs.core, javafx.base;
+	exports application;
 }
